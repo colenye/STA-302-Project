81ef8fe9455dd13a7123d0db4efbe0e88cf035f1
bluknight31 <andrew.rods@gmail.com>
2026-02-12 13:12:28 -0500

    Update Preliminary.R
    The code has been wrapped in a for loop to make it a bit cleaner and streamline the process of including any new data sets for additional samples.

--- Preliminary.R
+++ Preliminary.R
@@ -1,31 +1,35 @@
-szn25 = read.csv("szn25.txt")
-szn26 = read.csv("szn26.txt")
-szn25Advanced = read.csv("szn25Advanced.txt")
-szn26Advanced = read.csv("szn26Advanced.txt")
+seasons = c(25, 26)
 
-szn26$Rk = NULL
-szn25$Rk = NULL
+all_vars = list()
+all_bpm = list()
 
-szn26Vars = szn26[1:min(nrow(szn26), 500), c("Age", "Pos", "PTS", "ORB", "AST")]
-szn25Vars = szn25[1:min(nrow(szn25), 500), c("Age", "Pos", "PTS", "ORB", "AST")]
-szn26BPM = data.frame(BPM = szn26Advanced[1:min(nrow(szn26Advanced), 500), c("BPM")])
-szn25BPM = data.frame(BPM = szn25Advanced[1:min(nrow(szn25Advanced), 500), c("BPM")])
+for i in seq_along(seasons)) {
+  season = seasons[i]
 
+  predictor_source = read.csv(paste0("szn", season, ".txt"))
+  target_source = read.csv(paste0("szn", season, "Advanced.txt"))
+  basic$Rk = NULL
 
-table = rbind(szn26Vars, szn25Vars)
-BPMTable =  rbind(szn26BPM, szn25BPM)
+  # Avoid duplicate players within a season
+  basic_unique = basic[!duplicated(basic$player_id, fromLast = TRUE), ]
+  advanced_unique = advanced[!duplicated(advanced$player_id, fromLast = TRUE), ]
 
-
-
-if (nrow(table) == nrow(BPMTable)){
-  table = cbind(table, BPMTable)  
+  vars = basic_unique[1:min(nrow(basic_unique), 500), 
+                      c("player_id", "Age", "Pos", "PTS", "ORB", "AST")]
+  bpm = advanced_unique[1:min(nrow(advanced_unique), 500), c("player_id", "BPM")]
+  
+  all_vars[[i]] = vars
+  all_bpm[[i]] = bpm
 }
 
+table = do.call(rbind, all_vars)
+BPMTable = do.call(rbind, all_bpm)
 
+if (nrow(table) == nrow(BPMTable)) {
+  table = cbind(table, BPMTable)
+}
 
-model <- lm(BPM ~ Age + Pos + PTS + ORB + AST,data = table)
-
+model <- lm(BPM ~ Age + Pos + PTS + ORB + AST, data = table)
 summary(model)
-# Predict mpg for the new data
-predicted_mpg <- predict(model, data.frame(Age=50, Pos="PG", PTS = 3, ORB = 1, AST =5))
 
+predicted_bpm <- predict(model, data.frame(Age=50, Pos="PG", PTS=3, ORB=1, AST=5))
