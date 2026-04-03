6c2042799ffd4581b735ee17669ca1fc013287a9
bluknight31 <andrew.rods@gmail.com>
2026-04-03 18:58:36 -0400

    Update Preliminary.R

--- Preliminary.R
+++ Preliminary.R
@@ -1,8 +1,6 @@
 seasons = c(25, 26)
-
 all_vars = list()
 all_bpm = list()
-
 for (i in seq_along(seasons)) {
   season = seasons[i]
   
@@ -17,91 +15,78 @@ for (i in seq_along(seasons)) {
   advanced_unique = target_source[!duplicated(target_source$player_id, fromLast = TRUE), ]
   
   vars = basic_unique[1:min(nrow(basic_unique), 500),
-                      c("player_id", "Age", "Pos", "PTS", "ORB", "AST", "STL")]
+                      c("player_id", "Age", "Pos", "PTS", "ORB", "AST", "STL", "BLK")]
   bpm = advanced_unique[1:min(nrow(advanced_unique), 500), c("player_id", "BPM")]
+  
+  vars$season = season
+  bpm$season = season
   
   all_vars[[i]] = vars
   all_bpm[[i]] = bpm
 }
-
 nba_data = do.call(rbind, all_vars)
 BPMTable = do.call(rbind, all_bpm)
-
-nba_data = merge(nba_data, BPMTable, by = "player_id")
+nba_data = merge(nba_data, BPMTable, by = c("player_id", "season"))
 nba_data$BPM = as.numeric(nba_data$BPM)
 nba_data = nba_data[!is.na(nba_data$BPM), ]
 
-
 # ---- Transformations --------------------------------------------------------
-
-model_raw = lm(BPM ~ Age + Pos + PTS + ORB + AST + STL, data = nba_data)
-
+model_raw = lm(BPM ~ Age + Pos + PTS + ORB + AST + STL + BLK, data = nba_data)
 png("diagnostic_plots_baseline.png", width = 10, height = 10, units = "in", res = 300)
 par(mfrow = c(2, 2))
 plot(model_raw, main = "Baseline Model Diagnostics")
 dev.off()
 
 library(MASS)
-
 shift <- abs(min(nba_data$BPM)) + 1
 nba_data$BPM_shifted <- nba_data$BPM + shift
-
-model_for_bc = lm(BPM_shifted ~ Age + Pos + PTS + ORB + AST + STL, data = nba_data)
-
+model_for_bc = lm(BPM_shifted ~ Age + Pos + PTS + ORB + AST + STL + BLK,
+                  data = nba_data)
 png("boxcox_plot.png", width = 8, height = 5, units = "in", res = 300)
 b <- boxcox(model_for_bc, lambda = seq(-3, 3, by = 0.1))
 dev.off()
-
 lambda <- b$x[which.max(b$y)]
 cat("Optimal Box-Cox lambda:", lambda, "\n")
-
 lambda_rounded <- round(lambda)
 cat("Rounded lambda used for transformation:", lambda_rounded, "\n")
-
 geom_mean <- exp(mean(log(nba_data$BPM_shifted)))
 nba_data$BPM_bc <- geom_mean^(1 - lambda_rounded) *
   (nba_data$BPM_shifted^lambda_rounded - 1) / lambda_rounded
-
-model_bc = lm(BPM_bc ~ Age + Pos + PTS + ORB + AST + STL, data = nba_data)
-
-cat("\nAIC Comparison:\n")
+model_bc = lm(BPM_bc ~ Age + Pos + PTS + ORB + AST + STL + BLK, data = nba_data)
+cat("\nAIC Comparison (raw vs Box-Cox):\n")
 AIC(model_raw, model_bc)
 
-
 # ---- Outlier Screening ------------------------------------------------------
-
 cooks_d = cooks.distance(model_raw)
 stud_resid = rstudent(model_raw)
 leverage = hatvalues(model_raw)
-
 remove_idx = which(cooks_d > (4 / nrow(nba_data)) & abs(stud_resid) > 3)
 nba_data_clean = nba_data[-remove_idx, ]
-
 cat("Observations removed:", length(remove_idx), "\n")
 cat("Remaining observations:", nrow(nba_data_clean), "\n")
 
-
 # ---- Predictor Selection ----------------------------------------------------
-
-model = lm(BPM ~ Age + Pos + PTS + ORB + AST + STL, data = nba_data_clean)
+model = lm(BPM ~ Age + Pos + PTS + ORB + AST + STL + BLK, data = nba_data_clean)
 summary(model)
 
-model_noSTL = lm(BPM ~ Age + Pos + PTS + ORB + AST, data = nba_data_clean)
-
+# Test whether removing STL worsens model
+model_noSTL = lm(BPM ~ Age + Pos + PTS + ORB + AST + BLK, data = nba_data_clean)
 AIC(model_noSTL, model)
 summary(model_noSTL)$adj.r.squared
 summary(model)$adj.r.squared
 
-
 # ---- Diagnostic Plots -------------------------------------------------------
-
 png("cooks_distance.png", width = 8, height = 5, units = "in", res = 300)
 plot(cooks_d, type = "h",
      ylab = "Cook's Distance", xlab = "Observation Index",
      main = "Cook's Distance - Baseline Model")
 abline(h = 4 / nrow(nba_data), col = "red", lty = 2)
-legend("topright", legend = c("4/n", "16/n"),
-       col = c("red", "orange"), lty = 2, cex = 0.8)
+legend("topright", legend = "4/n", col = "red", lty = 2, cex = 0.8)
+dev.off()
+
+png("diagnostic_plots_baseline.png", width = 10, height = 10, units = "in", res = 300)
+par(mfrow = c(2, 2))
+plot(model_raw, main = "Baseline Model Diagnostics")
 dev.off()
 
 png("diagnostic_plots_final.png", width = 10, height = 10, units = "in", res = 300)
